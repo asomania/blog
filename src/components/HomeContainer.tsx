a1f15f9012c5818f85f31fdf067cfe71d16ea81d
asomania <erenkucuk2002@gmail.com>
2024-12-12 00:30:37 +0300

    changed with api response

--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -2,56 +2,16 @@ import { BsDot } from "react-icons/bs";
 import "./styles/home-container.css";
 import { useNavigate } from "react-router-dom";
 import api from "../api";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Blog } from "../type/Blog";
 
 const HomeContainer = () => {
-  const items = [
-    {
-      name: "Esnaf",
-      date: "12.2024",
-      header: "Blog Header 1",
-      details:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi, dicta.",
-      image: "avatar.jpg",
-    },
-    {
-      name: "Asomania Dev",
-      date: "01.2025",
-      header: "Blog Header 2",
-      details:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
-      image: "avatar.jpg",
-    },
-    {
-      name: "John Doe",
-      date: "02.2025",
-      header: "Blog Header 3",
-      details:
-        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.",
-      image: "avatar.jpg",
-    },
-    {
-      name: "Jane Smith",
-      date: "03.2025",
-      header: "Blog Header 4",
-      details:
-        "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.",
-      image: "avatar.jpg",
-    },
-    {
-      name: "Alice Johnson",
-      date: "04.2025",
-      header: "Blog Header 5",
-      details:
-        "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
-      image: "avatar.jpg",
-    },
-  ];
+  const [items, setItems] = useState<Blog[]>([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await api.get("/blogs");
-
+        setItems(response.data);
         // Burada veriyi işleyebilirsiniz
         console.log(response.data);
       } catch (error) {
@@ -70,7 +30,7 @@ const HomeContainer = () => {
           <div
             className="cursor d-flex columns gap-15-em"
             key={index}
-            onClick={() => navigate(`/about/${index}`)}
+            onClick={() => navigate(`/about/${index + 1}`)}
           >
             <div className="d-flex a-center ibm-font gap-15-em">
               <img
@@ -78,17 +38,17 @@ const HomeContainer = () => {
                 alt=""
                 className="logo-avatar d-flex a-center j-center"
               />
-              <p className="grey-text">{item.name}</p>
+              <p className="grey-text">Eren Küçük</p>
               <BsDot />
-              <p className="grey-text">{item.date}</p>
+              <p className="grey-text">2024</p>
             </div>
             <div className="d-flex gap-25">
               <div className="d-flex gap-25">
-                <img src={item.image} alt="" className="blog-small-image" />
+                <img src="avatar.jpg" alt="" className="blog-small-image" />
                 <div className="d-flex columns gap-25 j-center">
-                  <p className="bold ibm-font">{item.header}</p>
+                  <p className="bold ibm-font">{item.title}</p>
                   <p className="grey-text source-font text-16">
-                    {item.details}
+                    {item.content}
                   </p>
                 </div>
               </div>

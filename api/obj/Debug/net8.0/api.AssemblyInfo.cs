1ce4a81017bd878b4914be4424e13197a5442baa
asomania <erenkucuk2002@gmail.com>
2025-03-02 22:35:39 +0300

    port changed

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb13b602c95e3c6c687979da49aa20f867bfd09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1f15f9012c5818f85f31fdf067cfe71d16ea81d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
